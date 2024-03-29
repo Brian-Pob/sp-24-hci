4a4e7525ec7029f48a72340452f289e16c4548ec
Brian-Pob <brianpoblete77777@gmail.com>
2024-03-29 14:34:54 -0400

    add components

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
-import { fileURLToPath } from 'url';
+import path from 'node:path'
+import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite(),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
