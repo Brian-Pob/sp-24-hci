84013dc2b35ab102c4e95900d88d48fd4c9118a3
Brian-Pob <brianpoblete77777@gmail.com>
2024-04-02 00:22:05 -0400

    implement basic search

--- src/routes/search.results.jsx
+++ src/routes/search.results.jsx
@@ -1,17 +1,36 @@
 import { createFileRoute } from "@tanstack/react-router";
-
+import { Event } from "@/components/event";
 export const Route = createFileRoute("/search/results")({
+	loaderDeps: ({ search: { searchText } }) => ({ searchText }),
+	loader: async ({ deps: { searchText } }) =>
+		fetchSearchResults({ searchText }),
 	component: SearchResults,
 });
 
+async function fetchSearchResults({ searchText }) {
+	const allEvents = await fetch("/api/events.json").then((res) => res.json());
+	const searchResults = allEvents.filter((event) =>
+		event.title.toLowerCase().includes(searchText),
+	);
+	return { searchResults };
+}
+
 function SearchResults() {
-	const { searchText, searchTags, searchFilters } = Route.useSearch();
+	// const { searchTags, searchFilters } = Route.useSearch();
+	const { searchText } = Route.useLoaderDeps();
+	const { searchResults } = Route.useLoaderData();
 	return (
-		<div>
-			Hello /search-results!
-			<p>searchText: {searchText}</p>
-			<p>searchTags: {searchTags}</p>
-			<p>searchFilters: {searchFilters}</p>
+		<div className="overflow-y-scroll w-full pb-4">
+			<h1 className="text-4xl font-medium p-4">
+				Events matching &quot;{searchText}&quot;
+			</h1>
+			<ul className="flex flex-col gap-4 px-4">
+				{searchResults.map((event) => (
+					<li key={event.id} className="shadow-lg rounded-lg">
+						<Event event={event} />
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
